e2b66e92a4b2fce4b0c407db8da48c5bfb818dd0
Wildan <wildanalifrio@gmail.com>
2024-06-29 19:19:41 +0700

    Initial Commit

---
+++ dicoding-pemula/dicoding_pemulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicoding_pemulaApp.swift
+//  dicoding-pemula
+//
+//  Created by Wildan on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct dicoding_pemulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
